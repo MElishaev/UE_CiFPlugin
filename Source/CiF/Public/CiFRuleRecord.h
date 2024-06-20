325183e39a47b3867424e0849bb9b1cb4a840d68
MElishaev <secret678@gmail.com>
2024-06-20 18:57:04 +0300

    Added CiFRuleRecord class

---
+++ Source/CiF/Public/CiFRuleRecord.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "CiFRuleRecord.generated.h"
+
+class UCiFInfluenceRule;
+
+UENUM()
+enum class ERuleRecordType
+{
+	MICROTHEORY,
+	SOCIAL_EXCHANGE
+};
+
+/**
+ * A record of influence rules that fire when forming intent. For use for displaying rules that were true;
+ */
+UCLASS()
+class CIF_API UCiFRuleRecord : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UCiFInfluenceRule* mInfluenceRule;
+	ERuleRecordType mType;
+	FName mName;
+	FName mInitiator;
+	FName mResponder;
+	FName mOther;
+};
