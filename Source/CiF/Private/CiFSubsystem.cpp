c0fa89d86848442d3753d60971983ff46e2fa200
MElishaev <secret678@gmail.com>
2024-07-01 21:10:02 +0300

    Added implementation of getting instance of CiFManager in the CiFSubsystem

--- Source/CiF/Private/CiFSubsystem.cpp
+++ Source/CiF/Private/CiFSubsystem.cpp
@@ -2,3 +2,14 @@
 
 
 #include "CiFSubsystem.h"
+#include "CiFManager.h"
+
+UCiFManager* UCiFSubsystem::getInstance()
+{
+	if (mCiFInstance) {
+		return mCiFInstance;
+	}
+	
+	mCiFInstance = NewObject<UCiFManager>();
+	return mCiFInstance;
+}
